ab4f31f17d6e73af32be06b1856cacde2ad8f6dc
geruz <geruz@mail.ru>
2017-05-24 22:16:19 +0700

    add readme.md

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const log = require('./lib/log');
-const aurum = require('./lib/aurum');
+const Aurum = require('./lib/aurum');
 const {Controller} = require('./lib/caller');
 const RouteContext = require('./lib/route/context');
 
 module.exports = {
     log,
-    Aurum: aurum,
+    Aurum,
     Controller,
     RouteContext,
 };
